c3aee06259b3304694cb829348ed50b069d7b022
brettimus <brbeut@gmail.com>
2015-06-02 01:17:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# html5 boilerplate (cli)
+:runner: this was just a quick script, but it'd be fun to add some options to it!
+
+
+Also, considering taking the 5 out of the command because it's annoying. Thoughts?
+## Usage
+It's dead easy!
+
+Copy all html5 bp into `./pretty-web-site`.
+```sh
+mkdir pretty-web-site
+cd pretty-web-site
+html5bp
+```
+
+Copy all html5 bp into `/path/to/pretty-web-site`
+```sh
+html5bp /path/to/pretty-web-set
+```
+
+## Use your own templates
+Place the templates you'd like to use in `~/.html5bp/*`. (Sorry, Windows folk, but this will won't work for ya.)
+
---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+var ncp = require("ncp").ncp,
+    path = require("path"),
+    expandHomeDir = require("expand-home-dir");
+
+var copyTo = process.argv[2],
+    source = path.join(__dirname, "html5bp"),
+    dest;
+
+    if (copyTo) {
+        if (path.isAbsolute(copyTo)) {
+            dest = copyTo;
+        }
+        else {
+            dest = path.join(process.cwd(), dest);
+        }
+    }
+    else {
+        dest = process.cwd();
+    }
+
+// TODO - make platform agnostic
+ncp(expandHomeDir("~/.html5bp"), dest, function(err, data) {
+    if (err) {
+        console.log(err);
+        ncp(source, dest, function(err, data) {
+            if (err) console.log("Error: ", err);
+            else printDone(dest);
+        });
+    } else {
+        printDone(dest);
+    }
+});
+
+function printDone(dest) {
+    console.log("HTML5 Boilerplate copied to "+dest);
+}
+
